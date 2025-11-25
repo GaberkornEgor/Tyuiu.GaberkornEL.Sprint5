3df76d02dfd9372f563b5e471f485abb83620f34
Ymir4ik <middem4@gmail.com>
2025-11-25 21:30:15 +0500

    п

---
+++ Tyuiu.GaberkornEL.Sprint5.Task6.V11.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.GaberkornEL.Sprint5.Task6.V11.Lib;
+using System.IO;
+namespace Tyuiu.GaberkornEL.Sprint5.Task6.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\Ymir\Downloads\Sprint5Task6\InPutDataFileTask6V11.txt";
+
+            var res = ds.LoadFromDataFile(path);
+            int wait = 3;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string path = @"C:\Users\Ymir\Downloads\Sprint5Task6\InPutDataFileTask6V11.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.GaberkornEL.Sprint5.Task6.V11/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.GaberkornEL.Sprint5.Task6.V11.Lib;
+namespace Tyuiu.GaberkornEL.Sprint5.Task6.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\Ymir\Downloads\Sprint5Task6\InPutDataFileTask6V11.txt";
+            Console.Title = "Спринт #5 | Выполнил: Габеркорн Егор Леонидович | АСОиУб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс file. Обработка текстовых файлов                            *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Габеркорн Егор Леонидович | АСОиУб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл, в котором есть набор символьных данных.                       *");
+            Console.WriteLine("* Найти кол-во слов длинной шесть символов в заданной строке              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+
+
+            Console.WriteLine(" ***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
