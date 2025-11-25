2bad5ea8cbf9f4a84ba0ba398d363a58a06c86cb
Ymir4ik <middem4@gmail.com>
2025-11-25 19:58:04 +0500

    п

---
+++ Tyuiu.GaberkornEL.Sprint5.Task3.V4.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿namespace Tyuiu.GaberkornEL.Sprint5.Task3.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckFile()
+        {
+            string path = @"C:\Users\Ymir\AppData\Local\Temp\OutPutFileTask3.bin";
+
+            FileInfo file = new FileInfo(path);
+            bool fileExists = file.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.GaberkornEL.Sprint5.Task3.V4/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.GaberkornEL.Sprint5.Task3.V4.Lib;
+
+namespace Tyuiu.GaberkornEL.Sprint5.Task3.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт | Выполнил: Габеркорн Егор Леонидович | АСОиУб-25-1";
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* Спринт #5                                                     *");
+            Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл           *");
+            Console.WriteLine("* Задание #3                                                    *");
+            Console.WriteLine("* Вариант #4                                                    *");
+            Console.WriteLine("* Выполнил : Габеркорн Егор Леонидович | АСОиУб-25-1            *");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* УСЛОВИЕ :                                                     *");
+            Console.WriteLine("* Дано выражение. Вычислить его значение при x = 3. Результат   *");
+            Console.WriteLine("* сохранить в бинарный файл. Результат округлить до 3 знаков    *");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ :                                             *");
+            Console.WriteLine("*         x + 1                                                 *");
+            Console.WriteLine("* y = ln -------                                                *");
+            Console.WriteLine("*         x + 2                                                 *");
+            Console.WriteLine("*****************************************************************");
+
+            int x = 3;
+
+            var res = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                    *");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine($"* Файл: {ds.SaveToFileTextData(x)} ");
+            Console.WriteLine($"* Создан!                                                      *");
+            Console.WriteLine("*****************************************************************");
+            Console.ReadKey();
+
+        }
+    }
+}
