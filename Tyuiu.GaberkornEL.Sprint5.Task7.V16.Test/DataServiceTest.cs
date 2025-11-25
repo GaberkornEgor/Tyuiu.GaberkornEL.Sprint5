f7d0f5f7907cdf135e3459b1f75d9ec22d169453
Ymir4ik <middem4@gmail.com>
2025-11-25 21:50:55 +0500

    t

---
+++ Tyuiu.GaberkornEL.Sprint5.Task7.V16.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GaberkornEL.Sprint5.Task7.V16.Lib;
+namespace Tyuiu.GaberkornEL.Sprint5.Task7.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void LoadDataAndSave()
+        {
+            string path = @"C:\Users\Ymir\Downloads\Sprint5Task7\InPutDataFileTask7V16.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.GaberkornEL.Sprint5.Task7.V16/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.GaberkornEL.Sprint5.Task7.V16.Lib;
+namespace Tyuiu.GaberkornEL.Sprint5.Task7.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Файл находится по пути ...\\OutPutDataFileTask7V16.txt");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            ds.LoadDataAndSave(@"C:\Users\Ymir\Downloads\Sprint5Task7\InPutDataFileTask7V16.txt");
+            string text = File.ReadAllText(Path.Combine(new string[] { Path.GetTempPath(), "OutPutDataFileTask7V16.txt" }));
+            Console.WriteLine(text);
+
+
+
+        }
+
+
+    }
+
+
+
+}
