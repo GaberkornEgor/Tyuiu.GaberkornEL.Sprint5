eaceac12ae3e66bc4820e7d42bb3fafe113e8b0d
Ymir4ik <middem4@gmail.com>
2025-11-25 20:34:19 +0500

    g

---
+++ Tyuiu.GaberkornEL.Sprint5.Task4.V2/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.GaberkornEL.Sprint5.Task4.V2.Lib;
+namespace Tyuiu.GaberkornEL.Sprint5.Task4.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Габеркорн Егор Леонидович | АСОиУб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнила: Габеркорн Егор Леонидович | АСОиУб-25-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл, в котором есть вещественное значение. Прочитать значение из   *");
+            Console.WriteLine("* файла и подставить вместо Х в формулу.                                  *");
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            string path = @"C:\Users\Ymir\Downloads\Sprint5Task4\InPutDataFileTask4V2.txt";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Данные находятся в файле: " + path);
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
