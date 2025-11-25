70bc9d1c81a79c5e9a6a1086b8a68cdac6764368
Ymir4ik <middem4@gmail.com>
2025-11-25 17:55:23 +0500

    Add project files.

---
+++ Tyuiu.GaberkornEL.Sprint5.Task0.V10.Lib/ISprint5Task0V10.cs
@@ -0,0 +1,6 @@
+﻿namespace Tyuiu.GaberkornEL.Sprint5.Task0.V10.Lib
+{
+    public interface ISprint5Task0V10
+    {
+    }
+}
---
+++ Tyuiu.GaberkornEL.Sprint5.Task0.V10/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.GaberkornEL.Sprint5.Task0.V10.Lib;
+
+DataService ds = new DataService();
+Console.Title = "Спринт #5 | Выполнил: Габеркорн Егор Леонидович | АСОиУб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #5                                                               *");
+Console.WriteLine("* Тема:  Класс File. Запись данных в текстовый файл                       *");
+Console.WriteLine("* Задание #0                                                              *");
+Console.WriteLine("* Вариант #10                                                             *");
+Console.WriteLine("* Выполнил: Габеркорн Егор Леонидович | АСОиУб-25-1                       *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дано выражение F(x) = 1.6x^3-2.1x^2+7*x, вычислить его значение при     *");
+Console.WriteLine("* x = 2, результат сохранить в текстовый файл OutPutFileTask0.txt и       *");
+Console.WriteLine("* вывести на консоль. Округлить до трёх знаков после запятой.             *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+int x = 2;
+Console.WriteLine("x = " + x);
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+string res = ds.SaveToFileTextData(x);
+Console.WriteLine("Файл: " + res);
+Console.WriteLine("Создан!");
+Console.ReadKey();
